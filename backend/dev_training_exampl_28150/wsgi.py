4fd864bc9b0b03cb558ab0135bec6726ad383917
Crowdbotics <team@crowdbotics.com>
2021-06-21 21:49:13 +0000

    Update models

--- backend/dev_training_exampl_28150/wsgi.py
+++ backend/dev_training_exampl_28150/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dev_training_exampl_28150.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dev_training_exampl_28150.settings")
 
 application = get_wsgi_application()
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,11 @@
+from django.conf import settings
 from django.db import models
+
+
+class Entry(models.Model):
+    "Generated Model"
+    body = models.TextField()
+    title = models.TextField(
+        null=True,
+        blank=True,
+    )
