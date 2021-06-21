b712f0a6c3f8acd55a210bf6ba6c8770a57ff7e1
Crowdbotics <team@crowdbotics.com>
2021-06-21 21:41:11 +0000

    Initial commit.

---
+++ backend/dev_training_exampl_28150/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dev_training_exampl_28150 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dev_training_exampl_28150.settings')
+
+application = get_wsgi_application()
